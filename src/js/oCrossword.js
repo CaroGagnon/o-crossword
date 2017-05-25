b77b41f6cff9342c40674d83f9e0e7e104e63e0b
Sean Tracey <seanmtracey@gmail.com>
2017-05-25 14:05:14 +0100

    Bind cousins (grid elements) to clue elements.

--- src/js/oCrossword.js
+++ src/js/oCrossword.js
@@ -14,6 +14,77 @@ function prevAll(node) {
 	return nodes.slice(0, pos);
 };
 
+function getCousins(element, type, num, direction, offset){
+
+	if(type === 'clue'){
+
+		const gridInput = document.body.querySelector('[data-o-crossword-number="' + num + '"]');
+
+		let TD;
+
+		if(direction === 'across'){
+			const TDs = gridInput.parentNode.querySelectorAll('td');
+			let answerStartOffset = 0;
+			let count = 0;
+
+			for(let idx = 0; idx < TDs.length; idx += 1){
+				const td = TDs[idx];
+
+				if(td === gridInput){
+					answerStartOffset = idx;
+				}
+
+				if(count === offset){
+					TD = td;
+					break
+				} else {
+					count += 1;
+				}
+
+			}
+
+		} else if(direction === 'down'){
+
+			const acrossOffset = Array.from(gridInput.parentNode.querySelectorAll('td'))
+				.map( (td, idx) => {
+					return td === gridInput ? idx : false;
+				})
+				.filter(result => {
+					return result !== false;
+				})[0]
+			;
+
+			const columnStart = Number( gridInput.parentNode.getAttribute('data-tr-index') );
+
+			TD = Array.from(document.querySelectorAll('table tr'))
+				.filter( (el, idx) => {
+					return idx >= columnStart;
+				})
+				.map(row => {
+					return row.querySelector(`td:nth-child(${acrossOffset + 1})`);
+				})[offset]
+			;
+
+		}
+
+		return {
+			'td' : TD
+		};
+
+	}
+
+	if(type === 'grid'){
+
+	}
+
+	if(type === 'navigation'){
+
+	}
+
+	return null;
+
+}
+
 function writeErrorsAsClues(rootEl, json) {
 	const cluesEl = rootEl.querySelector('ul.o-crossword-clues');
 
@@ -63,6 +134,7 @@ function buildGrid(
 		tr.setAttribute('data-tr-index', i);
 		for (let j=0; j<cols; j++) {
 			const td = document.createElement('td');
+
 			tr.appendChild(td);
 			if (gridnums[i][j]) {
 				td.dataset.oCrosswordNumber = gridnums[i][j];
@@ -111,12 +183,24 @@ function buildGrid(
 			tempLi.dataset.oCrosswordClueId = index;
 
 			for(var i = 0; i < answerLength; ++i) {
-				let tempInput = document.createElement('input');
-				tempInput.setAttribute('maxlength', 1);
-				tempInput.setAttribute('data-link-identifier', 'A' + across[0] + '-' + i);
+				let tempInput = document.createElement('span');
+				tempInput.dataset.offset = i;
+				tempInput.cousins = getCousins(this, 'clue', across[0], 'across', Number( tempInput.dataset.offset ));
+
+				tempInput.addEventListener('click', function(){
+
+					console.log(this.cousins);
+
+					document.querySelectorAll('span[data-active="true"]').forEach(span => {
+						span.dataset.active = 'false';
+					});
+					tempInput.dataset.active = 'true';
+
+				}, false);
+
 				let count = 0;
-				
-				if(across[3].length > 1) {	
+
+				if(across[3].length > 1) {
 					for(var j = 0; j < across[3].length; ++j) {
 						if(j%2 === 1) {
 							count += parseInt(across[3][j-1]);
@@ -158,13 +242,24 @@ function buildGrid(
 			tempLi.dataset.oCrosswordClueId = clues.across.length + index;
 
 			for(var i = 0; i < answerLength; ++i) {
-				let tempInput = document.createElement('input');
-				tempInput.setAttribute('maxlength', 1);
-				tempInput.setAttribute('data-link-identifier', 'D' + down[0] + '-' + i);
+				let tempInput = document.createElement('span');
+				tempInput.dataset.offset = i;
+				tempInput.cousins = getCousins(this, 'clue', down[0], 'down', Number( tempInput.dataset.offset ));
 
+				tempInput.addEventListener('click', function(){
+
+					console.log(this.cousins);
+
+					document.querySelectorAll('span[data-active="true"]').forEach(span => {
+						span.dataset.active = 'false';
+					});
+					tempInput.dataset.active = 'true';
+
+				}, false);
+				
 				let count = 0;
 
-				if(down[3].length > 1) {	
+				if(down[3].length > 1) {
 					for(var j = 0; j < down[3].length; ++j) {
 						if(j%2 === 1) {
 							count += parseInt(down[3][j-1]);
@@ -371,294 +466,14 @@ OCrossword.prototype.assemble = function assemble() {
 		const magicInput = document.createElement('input');
 		gridScaleWrapper.appendChild(magicInput);
 		magicInput.classList.add('o-crossword-magic-input');
-		let magicInputTargetEl = null;
-		let magicInputNextEls = null;
+
 		magicInput.type = 'text';
 		magicInput.style.display = 'none';
-
-		let blockHighlight = false;
-		let previousClueSelection = null;
-
-		this.addEventListener(magicInput, 'keydown', function (e) {
-			if (!isAndroid()) {
-				e.preventDefault();
-			}
-
-			if (e.keyCode === 13) { //enter
-				magicInputNextEls = null;
-				return progress();
-			}
-			if (
-				e.keyCode === 9 || //tab
-				e.keyCode === 40 ||//down
-				e.keyCode === 39 ||//right
-				e.keyCode === 32 //space
-			) {
-				return progress();
-			}
-			if (
-				e.keyCode === 37 || //left
-				e.keyCode === 38 //up
-			) {
-				return progress(-1);
-			}
-			if (
-				e.keyCode === 8 //backspace
-			) {
-				magicInput.value = '';
-				return progress(-1);
-			}
-
-			if( e.keyCode === 16 || //shift
-				e.keyCode === 20 || //caps lock
-				e.keyCode === 91 	//Command
-			) {
-				magicInput.value = '';
-				return;
-			}
-
-			if(!isAndroid()) {
-				magicInput.value = String.fromCharCode(e.keyCode);
-
-				if( e.keyCode === 229) {
-					//fix safari press down
-					magicInput.value = '';
-					return;
-				}
-			}
-			
-			progress();
-		});
-
-		this.addEventListener(cluesEl, 'keydown', function(e){
-			let timer = 5;
-
-			if (!isAndroid()) {
-				e.preventDefault();
-				timer = 0;
-			}
-			
-			let gridSync = getCellFromClue(e.target);
-			
-
-			if (e.keyCode === 13) { //enter
-				e.target.blur();
-				return;
-			}
-			if (
-				e.keyCode === 9 || //tab
-				e.keyCode === 40 ||//down
-				e.keyCode === 39 ||//right
-				e.keyCode === 32 //space
-			) {
-				return nextInput(e.target, 1);
-			}
-			if (
-				e.keyCode === 37 || //left
-				e.keyCode === 38 //up
-			) {
-				return nextInput(e.target, -1);
-			}
-			if (
-				e.keyCode === 8 //backspace
-			) {
-				setTimeout(function(){
-					e.target.value = '';
-					gridSync.grid.textContent = e.target.value;
-					
-					if(gridSync.defSync) {
-						let defSync = cluesEl.querySelector('input[data-link-identifier="' + gridSync.defSyncInput +'"]');
-						defSync.value = e.target.value;
-					}
-
-					nextInput(e.target, -1);
-				}, timer);
-				
-				return;
-			}
-
-			if( e.keyCode === 16 || //shift
-				e.keyCode === 20 || //caps lock
-				e.keyCode === 91 	//Command
-			) {
-				e.target.value = '';
-				return;
-			}
-
-			if(!isAndroid()) {
-				e.target.value = String.fromCharCode(e.keyCode);
-
-				if( e.keyCode === 229) {
-					//fix safari press down
-					e.target.value = '';
-					return;
-				}
-			}
-
-			setTimeout(function(){
-				gridSync.grid.textContent = e.target.value;
-				
-				if(gridSync.defSync) {
-					let defSync = cluesEl.querySelector('input[data-link-identifier="' + gridSync.defSyncInput +'"]');
-					defSync.value = e.target.value;
-				}
-
-				nextInput(e.target, 1);
-			}, timer);
-		});
-
-		function nextInput(source, direction) {
-			let inputID = source.getAttribute('data-link-identifier');
-			let inputGroup = document.querySelectorAll('input[data-link-identifier^="' + inputID.split('-')[0] +'-"]');
-			let currentInput = inputID.split('-')[1];
-			let newInput = (direction === 1)?++currentInput:--currentInput;
-
-			if(newInput >= 0 && newInput < inputGroup.length) {
-				let next = cluesEl.querySelector('input[data-link-identifier="' + inputID.split('-')[0] +'-'+ newInput+'"]');
-				next.focus();
-				next.select();
-			} else {
-				source.blur();
-				let def = source.parentElement.parentElement;
-				def.click();
-			}
-		}
-
-		function getCellFromClue(clue) {
-			let inputIdentifier = clue.getAttribute('data-link-identifier');
-			let defDirection = (inputIdentifier.slice(0,1) === 'A')?'across':'down';
-			let defNum = inputIdentifier.slice(1,inputIdentifier.length).split('-')[0];
-			let defIndex = parseInt(inputIdentifier.split('-')[1]);
-			
-			let cells = gridEl.querySelectorAll('td:not(.empty)');
-			let selectedCell = {};
-
-			Array.from(cells).forEach(cell => {
-				let cellData = gridMap.get(cell);
-				for(let i = 0; i < cellData.length; ++i) {
-					if(
-						cellData[i].direction === defDirection &&
-						parseInt(cellData[i].number) === parseInt(defNum) &&
-						parseInt(cellData[i].answerPos) === parseInt(defIndex)
-					) {
-						selectedCell.grid = cell;
-						if(cellData.length > 1) {
-							selectedCell.defSync = true;
-
-							selectedCell.defSyncInput = constructInputIdentifier(cellData, defDirection);
-						}
-					}
-				}
-			});
-
-			return selectedCell;
-		}
-
-		function constructInputIdentifier(data, direction) {
-			let identifier;
-
-			for(let i = 0; i < data.length; ++i) {
-				if(data[i].direction !== direction) {
-					identifier = data[i].direction.slice(0,1).toUpperCase();
-					identifier += data[i].number;
-					identifier += '-';
-					identifier += data[i].answerPos;
-				}
-			}
-
-			return identifier;
-		}
-
-		const progress = debounce(function progress(direction) {
-			direction = direction === -1 ? -1 : 1;
-			const oldMagicInputEl = magicInputTargetEl;
-
-			if (
-				magicInputTargetEl &&
-				magicInput.value.match(/^[^\s]/)
-			) {
-				magicInputTargetEl.textContent = magicInput.value[0];
-			}
-
-			magicInputTargetEl = null;
-
-			if (magicInputNextEls) {
-				const index = magicInputNextEls.indexOf(oldMagicInputEl);
-				syncPartialClue(magicInput.value, magicInputNextEls, index);
-				if (magicInputNextEls[index + direction]) {
-					return takeInput(magicInputNextEls[index + direction], magicInputNextEls);
-				}
-			}
-
-			unsetClue(magicInputNextEls.length, direction);
-			magicInputNextEls = null;
-			magicInput.value = '';
-			blockHighlight = false;
-		}, 16);
-
-		this.addEventListener(magicInput, 'focus', magicInput.select());
-
-		const clueInputs = cluesEl.querySelectorAll('input');
-		this.addEventListener(clueInputs, 'focus', function(e){
-			magicInput.value ='';
-			magicInput.style.display = 'none';
-
-			let def = e.target.parentElement.parentElement;
-			let targetClue = {
-				'number': def.getAttribute('data-o-crossword-number'),
-				'direction': def.getAttribute('data-o-crossword-direction'),
-				'answerLength': def.getAttribute('data-o-crossword-answer-length')
-
-			};
-
-			previousClueSelection = targetClue;
-
-			if(!def.classList.contains('has-hover')) {
-				highlightGridByNumber(targetClue.number, targetClue.direction, targetClue.answerLength);
-			}
-
-		});
-
-		function takeInput(el, nextEls) {
-			if (
-				magicInputTargetEl &&
-				magicInput.value.match(/^[^\s]/)
-			) {
-				magicInputTargetEl.textContent = magicInput.value[0];
-			}
-
-			magicInput.style.display = '';
-
-			const oldClue = currentlySelectedGridItem;
-			const clues = gridMap.get(el);
-			if (!clues) return;
-			currentlySelectedGridItem = clues.find(item => (
-				item.direction === oldClue.direction &&
-				item.number === oldClue.number &&
-				item.answerLength === oldClue.answerLength
-			)) || currentlySelectedGridItem;
-
-			Array.from(gridEl.getElementsByClassName('receiving-input')).forEach(el => el.classList.remove('receiving-input'));
-			el.classList.add('receiving-input');
-			magicInput.value = el.textContent;
-			el.textContent = '';
-			magicInputTargetEl = el;
-			magicInputNextEls = nextEls;
-			magicInput.style.left = magicInputTargetEl.offsetLeft + 'px';
-			magicInput.style.top = magicInputTargetEl.offsetTop + 'px';
-
-			let timer = (isAndroid())?5:0;
-
-			setTimeout(function(){
-				magicInput.focus();
-				magicInput.select();
-			}, timer);
-		}
 
 		const onResize = function onResize(init) {
 			var isMobile = false;
 			const cellSizeMax = 40;
-			
+
 			if (window.innerWidth <= 739) {
 				isMobile = true;
 			} else if (window.innerWidth > window.innerHeight && window.innerHeight <=739 ) { //rotated phones and small devices, but not iOS
@@ -673,7 +488,6 @@ OCrossword.prototype.assemble = function assemble() {
 			let d2 = gridEl.getBoundingClientRect();
 			const width1 = d1.width;
 			const height1 = d1.height;
-			let width2 = d2.width;
 			const height2 = d2.height;
 
 			let scale = height2/height1;
@@ -729,258 +543,13 @@ OCrossword.prototype.assemble = function assemble() {
 			this.onResize = debounce(onResize, 100);
 		}
 
-		function highlightGridByCluesEl(el) {
-			if (blockHighlight) {
-				return;
-			}
-
-			while(el.parentNode) {
-				if (el.dataset.oCrosswordNumber) {
-					highlightGridByNumber(Number(el.dataset.oCrosswordNumber), el.dataset.oCrosswordDirection, el.dataset.oCrosswordAnswerLength);
-					return;
-				} else {
-					el = el.parentNode;
-				}
-			}
-			return false;
-		}
-
-		function setClue(number, direction) {
-			const el = cluesEl.querySelector(`li[data-o-crossword-number="${number}"][data-o-crossword-direction="${direction}"]`);
-			if (el) {
-				clueDisplayerText.textContent = el.querySelector('span').textContent;
-				const els = Array.from(cluesEl.getElementsByClassName('has-hover'));
-				els.filter(el2 => el2 !== el).forEach(el => el.classList.remove('has-hover'));
-				el.classList.add('has-hover');
-				currentClue = parseInt(el.getAttribute('data-o-crossword-clue-id'));
-			}
-		}
-
-		function highlightGridByNumber(number, direction, length) {
-			magicInput.style.display = 'none';
-			setClue(number, direction);
-			const els = Array.from(gridEl.querySelectorAll('td[data-o-crossword-highlighted]'));
-			for (const o of els) {
-				delete o.dataset.oCrosswordHighlighted;
-			}
-			const gridElsToHighlight = getGridCellsByNumber(gridEl, number, direction, length);
-			gridElsToHighlight.forEach(el => el.dataset.oCrosswordHighlighted = direction);
-		}
-
-		function unsetClue(number, direction) {
-			const el = cluesEl.querySelector(`li[data-o-crossword-number="${number}"][data-o-crossword-direction="${direction}"]`);
-			const els = Array.from(gridEl.querySelectorAll('td[data-o-crossword-highlighted]'));
-			
-			for (const o of els) {
-				delete o.dataset.oCrosswordHighlighted;
-			}
-
-			if (el) {
-				clueDisplayerText.textContent = '';
-				const els = Array.from(cluesEl.getElementsByClassName('has-hover'));
-				els.forEach(el => el.classList.remove('has-hover'));
-				el.classList.remove('has-hover');
-			}
-
-			magicInput.blur();
-			magicInput.style.display = 'none';
-		}
-
-		function syncPartialClue(letter, src, index) {
-			const gridItems = gridMap.get(src[index]);
-			let targets = [];
-
-			for(let i = 0; i < gridItems.length; ++i) {
-				let linkName = gridItems[i].direction[0].toUpperCase() + gridItems[i].number + '-' + gridItems[i].answerPos;
-				targets.push(cluesEl.querySelector('input[data-link-identifier="'+linkName+'"]'));
-			}
-			
-			Array.from(targets).forEach((target) => {
-				target.value = letter;
-			});
-		}
-
-		function isEquivalent(a, b) {
-		    var aProps = Object.getOwnPropertyNames(a);
-		    var bProps = Object.getOwnPropertyNames(b);
-
-		    if (aProps.length != bProps.length) {
-		        return false;
-		    }
-
-		    for (var i = 0; i < aProps.length; i++) {
-		        var propName = aProps[i];
-		        if (a[propName] !== b[propName]) {
-		            return false;
-		        }
-		    }
-
-		    return true;
-		}
-
-		function toggleClueSelection(clue) {
-			if (previousClueSelection !== null && isEquivalent(previousClueSelection, clue)) {
-				unsetClue(clue.number, clue.direction);
-				blockHighlight = false;
-				previousClueSelection = null;
-				return false;
-			}
-
-			blockHighlight = true;
-			previousClueSelection = clue;
-
-			return true;
-		}
-
-		const onTap = function onTap(e) {
-			let target;
-			let clueDetails;
-			let isNavigation = false;
-			blockHighlight = false;
-
-			if (e.target.nodeName === 'TD' || e.target.nodeName === 'INPUT') {
-				target = e.target;
-				blockHighlight = true;
-			} else {
-				let defEl;
-
-				if(e.target.nodeName === 'A') {
-					defEl = navigateClues(e);
-					isNavigation = true;
-				} else {
-					defEl = (e.target.nodeName === 'SPAN')?e.target.parentElement:e.target;
-				}
-
-				clueDetails = {};
-				clueDetails.number = defEl.getAttribute('data-o-crossword-number');;
-				clueDetails.direction = defEl.getAttribute('data-o-crossword-direction');
-				clueDetails.answerLength = defEl.getAttribute('data-o-crossword-answer-length');
-
-				if (!toggleClueSelection(clueDetails)) {
-					return;
-				}
-				
-				const el = gridEl.querySelector(`td[data-o-crossword-number="${clueDetails.number}"]`);
-				target = el;
-			}
-
-			if (target === magicInput) {
-				target = magicInputTargetEl;
-			}
-
-			if (gridEl.contains(target)) {
-				let cell = target;
-				while(cell.parentNode) {
-					if (cell.tagName === 'TD') {
-						break;
-					} else {
-						cell = cell.parentNode;
-					}
-				}
-				const clues = gridMap.get(cell);
-				if (!clues) {
-					return;
-				}
-
-				// cell.scrollIntoView(); //TODO: this works OK-ish for vertical oriented phones, could be explored
-
-				// iterate through list of answers associated with that cell
-				let index = clues.indexOf(currentlySelectedGridItem);
-
-				// if a new item is selected find what ever matches the current selection
-				if (index === -1 && currentlySelectedGridItem) {
-					const oldClue = currentlySelectedGridItem;
-
-					currentlySelectedGridItem = clues.find(item => (
-						item.direction === oldClue.direction &&
-						item.number === oldClue.number &&
-						item.answerLength === oldClue.answerLength
-					));
-				}
-
-				if (index !== -1 || !currentlySelectedGridItem) {
-					// the same cell has been clicked on again so
-					if (index + 1 === clues.length) index = -1;
-					currentlySelectedGridItem = clues[index + 1];
-				}
-
-				if (clueDetails !== undefined) {
-					currentlySelectedGridItem.number = clueDetails.number;
-					currentlySelectedGridItem.direction = clueDetails.direction;
-					currentlySelectedGridItem.answerLength = clueDetails.answerLength;
-				}
-
-				highlightGridByNumber(
-					currentlySelectedGridItem.number,
-					currentlySelectedGridItem.direction,
-					currentlySelectedGridItem.answerLength
-				);
-
-				if(!isNavigation) {
-					takeInput(cell, getGridCellsByNumber(
-						gridEl,
-						currentlySelectedGridItem.number,
-						currentlySelectedGridItem.direction,
-						currentlySelectedGridItem.answerLength
-					));
-				}
-			}
-		}.bind(this);
-
-		const navigateClues = function navigateClues (e) {
-			e.preventDefault();
-			const dir = (e.target === clueNavigationNext)?'forward':'backward';
-			const cluesTotal = parseInt(this.rootEl.parentElement.getAttribute('data-o-crossword-clue-length')) - 1;
-
-			if (dir === 'forward') {
-				++currentClue;
-
-				if(currentClue > cluesTotal) {
-					currentClue = 0;
-				}
-			} else {
-				--currentClue;
-				if(currentClue < 0) {
-					currentClue = cluesTotal;
-				}
-			}
-
-			return cluesEl.querySelector('li[data-o-crossword-clue-id="'+ currentClue +'"]');
-		}.bind(this);
-
-		this.addEventListener(cluesEl, 'mousemove', e => highlightGridByCluesEl(e.target));
-
-		this.rootEl.addEventListener('click', onTap, false);
-
 		onResize(true);
-		this.addEventListener(window, 'resize', this.onResize);
+		window.addEventListener('resize', onResize);
 	}
 
 	if(isiOS()) {
 		document.getElementsByTagName('body')[0].className += " iOS";
 	}
-};
-
-OCrossword.prototype.addEventListener = function(el, type, callback) {
-	if (this.listeners === undefined) {
-		this.listeners = [];
-	}
-
-	this.listeners.push({el, type, callback});
-
-	if(Object.prototype.toString.call(el) === '[object NodeList]') {
-		Array.from(el).forEach(function(element) {
-			element.addEventListener(type, callback);
-		});
-	} else {
-		el.addEventListener(type, callback);
-	}
-};
-
-OCrossword.prototype.removeAllEventListeners = function() {
-	this.listeners.forEach(function remove({el, type, callback}) {
-		el.removeEventListener(type, callback);
-	});
 };
 
 OCrossword.prototype.destroy = function destroy() {
